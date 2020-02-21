1939320515215980095881a71c7216626dea865b
daniel-paz2000 <60496702+daniel-paz2000@users.noreply.github.com>
2020-02-20 20:16:36 -0600

    Add files via upload

---
+++ practica2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* con dos variables enteras y dos flotantes hacer operaciones matematicas */
+
+
+int main() {
+int a=2, b=3;
+float c=3.5, d=4.5 ;
+char Q=4, R=5
+printf("%d+%d=%d",a,b, a+b);
+printf("%d-%d=%d",a,b, a-b);
+printf("%d*%d=%d",a,b, a*b);
+printf("%d/%d=%d",a,b, a/b);
+printf("%d%%d=%d",a,b, a%b);
+printf("%f+%f=%f",c,d, c+d);
+printf("%f-%f=%f",c,d, c-d);
+printf("%f*%f=%f",c,d, c*d);
+printf("%f/%f=%f",c,d, c/d);
+printf("%c>>4=%c",Q, q>>4);
+
+return 0;
+}
